8daf78a46861701f823ba202627af69ac8a5e23d
chrisli-sas <139907857+chrisli-sas@users.noreply.github.com>
2025-07-15 13:29:40 -0400

    add puppeteer

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { WebClient } from '@slack/web-api';
+import puppeteer from 'puppeteer';
 
 // Initialize Slack WebClient with your bot token
 const slackClient = new WebClient('');
@@ -8,21 +9,43 @@ export async function GET(request) {
   try {
     // const { channel, message } = await request.json();
 
-    const channel = '#casper-monitor-test';
-    const message = 'Hello World!';
+    // const channel = '#casper-monitor-test';
+    // const message = 'Hello World!';
+    //
+    // // Validate input
+    // if (!channel || !message) {
+    //   return NextResponse.json({ error: 'Channel and message are required' }, { status: 400 });
+    // }
+    //
+    // // Send message to Slack channel
+    // const result = await slackClient.chat.postMessage({
+    //   channel: channel, // Channel name (e.g., '#bot-test') or ID
+    //   text: message,
+    // });
+    //
+    // return NextResponse.json({ success: true, result }, { status: 200 });
 
-    // Validate input
-    if (!channel || !message) {
-      return NextResponse.json({ error: 'Channel and message are required' }, { status: 400 });
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
+
+    try {
+      const page = await browser.newPage();
+      await page.goto('https://casper.ca/en/mattresses', {
+        waitUntil: 'domcontentloaded',
+        args: [`--window-size=1400,1080`],
+        defaultViewport: {
+          width:1400,
+          height:1080
+        }
+      });
+      await page.waitForSelector('[href^="mattresses/the-casper"]');
+      await page.click('[href^="mattresses/the-casper"]');
+      await page.waitForSelector('.self-end.btn-primary.w-full');
+      await page.click('.self-end.btn-primary.w-full');
+      await page.waitForSelector('#non-exist');
+    } finally {
+      // await browser.close();
     }
-
-    // Send message to Slack channel
-    const result = await slackClient.chat.postMessage({
-      channel: channel, // Channel name (e.g., '#bot-test') or ID
-      text: message,
-    });
-
-    return NextResponse.json({ success: true, result }, { status: 200 });
+    return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
     console.error('Error sending Slack message:', error);
     return NextResponse.json({ error: 'Failed to send message' }, { status: 500 });
